d4e90ff8062c3b097f6488a6026613346fc47c11
Hong Xu <hong@8hobbies.com>
2025-01-20 20:16:51 -0800

    Rename retrieved_time to retrieval_time

---
+++ prisma/migrations/20250121041613_init/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "branches" (
+    "id" SERIAL NOT NULL,
+    "repository" TEXT NOT NULL,
+    "branch" TEXT NOT NULL,
+    "last_commit_retrieval_time" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "branches_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "commits" (
+    "branch_id" INTEGER NOT NULL,
+    "retrieval_time" TIMESTAMP(0) NOT NULL,
+    "commit_hash" TEXT NOT NULL,
+
+    CONSTRAINT "commits_pkey" PRIMARY KEY ("branch_id","retrieval_time")
+);
+
+-- CreateIndex
+CREATE INDEX "branches_last_commit_retrieval_time_index" ON "branches"("last_commit_retrieval_time");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "branches_repository_branch_key" ON "branches"("repository", "branch");
+
+-- AddForeignKey
+ALTER TABLE "commits" ADD CONSTRAINT "fk_branches" FOREIGN KEY ("branch_id") REFERENCES "branches"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
