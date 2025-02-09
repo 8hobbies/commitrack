90a4bc0565fa30364fb66264208c21cb86afcee3
Hong Xu <hong@8hobbies.com>
2025-02-09 15:45:11 -0800

    Use postgres sql dialect

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
       ((eval .
              (add-to-list 'exec-path
                           (concat (locate-dominating-file default-directory dir-locals-file)
-                                  "node_modules/.bin/"))))))
+                                  "node_modules/.bin/")))))
+ (sql-mode . ((eval . (sql-set-product 'postgres)))))
